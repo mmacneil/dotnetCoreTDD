0ce3f0cae090130204906f757aad81352f8d75af
Mark Macneil <mark.macneil@t4g.com>
2017-04-02 14:19:22 -0300

    post in progress

---
+++ TDDDemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDDDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
